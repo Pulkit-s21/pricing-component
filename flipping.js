d93b1e4574f58e8c1edfd3159834b9ae460be690
Pulkit Saxena <pulkitsaxena795@gmail.com>
2022-09-03 14:29:37 +0530

    changed js file

--- flipping.js
+++
@@ -1,8 +0,0 @@
-const valueNum = document.querySelectorAll(".value");
-const switchBtn = document.querySelector(".switch");
-
-switchBtn.addEventListener("click",() => {
-    valueNum.forEach(value=>{
-        value.classList.toggle("hidden");
-    });
-})
